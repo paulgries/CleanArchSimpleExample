79f643e0b33b2e6a9a59bf851bd29c2f64822781
Jonathan Calver <calver@cs.toronto.edu>
2021-10-13 14:23:21 -0400

    added comments and a unittest

--- src/edu/toronto/csc207/LoginInputBoundary.java
+++ src/edu/toronto/csc207/LoginInputBoundary.java
@@ -1,5 +1,7 @@
 package edu.toronto.csc207;
 
+//The interface that the logInUseCase implements
+//Note how it specifies what the input (arguments) and output (return type) are.
 public interface LoginInputBoundary {
-    public LoginUseCase.LoginResult logIn(String username, String password);
+    LoginUseCase.LoginResult logIn(String username, String password);
 }
